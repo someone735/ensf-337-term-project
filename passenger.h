a3f362da7ae1eea857e7b11b7fd3957a77086a09
someone735 <smeone83@gmail.com>
2023-11-21 16:46:36 -0700

    created other files

---
+++ passenger.h
@@ -0,0 +1,10 @@
+#ifndef MY_PASSENGER_CLASS
+#define MY_PASSENGER_CLASS
+#include <string>
+    class Passenger {
+        public:
+            Passenger();
+            Passenger(string fname, string lname, int phoneNumber);
+        private:
+    }
+#endif
