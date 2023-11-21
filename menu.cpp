0a5e52fd60cc0a1cf54c23b1ab502b86e4fb2b23
someone735 <smeone83@gmail.com>
2023-11-21 16:32:26 -0700

    creating seat and menu files

---
+++ menu.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Version 1.0" << endl;
+    cout << "Term Project - Flight Management Program in C++" << endl;
+    cout << "Produced by: Guntaas Uppal, John Tumacder, Noor ul Islam, Syed Waliullah" << endl;
+    cout << "Year: 2023" << endl;
+    while(1){
+    string s = "asdf";
+    cout << "Press return to continue" << endl;
+    cin >> s;
+    cout << "select option: 1, 2, 3, 4, 5, 6" << endl;
+    int x = 0;
+    x = 0;
+    cin>>x;
+    if (x != 1 || x != 2 || x != 3 || x != 4 || x != 5 || x != 6){
+        cout<<"hi"<<endl;
+        break;
+    }
+    break;
+    }
+}
---
+++ seat.h
@@ -0,0 +1,7 @@
+#ifndef MY_SEAT_CLASS
+#define MY_SEAT_CLASS
+    class Seat{
+        int a = 0;
+        
+    };
+#endif
