60f255a452507e7108e77ea458726869d47813af
someone735 <smeone83@gmail.com>
2023-11-23 15:42:10 -0700

    update

---
+++ flight.h
@@ -0,0 +1,11 @@
+#ifndef MY_FLIGHT_CLASS
+#define MY_FLIGHT_CLASS
+class Flight{
+    private:
+    string id;
+    int num_rows;
+    int num_cols;
+    vector <passenger> passengers;
+    vector <vector<seat>> seatmap;
+    }
+#endif
--- passenger.h
+++ passenger.h
@@ -1,10 +1,16 @@
 #ifndef MY_PASSENGER_CLASS
 #define MY_PASSENGER_CLASS
 #include <string>
+#include "seat.h"
     class Passenger {
         public:
             Passenger();
             Passenger(string fname, string lname, int phoneNumber);
         private:
+        string fname;
+        string lname;
+        string phone;
+        
+
     }
 #endif
