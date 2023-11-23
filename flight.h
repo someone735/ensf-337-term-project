d0b742f76602ae533521c533540e3f78136499a1
someone735 <smeone83@gmail.com>
2023-11-23 16:46:49 -0700

    updating menu

--- flight.h
+++ flight.h
@@ -1,10 +1,12 @@
 #ifndef MY_FLIGHT_CLASS
 #define MY_FLIGHT_CLASS
 #include <iostream>
+#include <vector>
 class Flight{
     public:
         Flight();
-        
+        Flight(int num_rows, int num_cols, vector <passengers> passengers, vector <vector<seat>> seatmap);
+        vector display_seatmap;
     private:
     string id;
     int num_rows;
