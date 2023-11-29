080b7deae7c4e42a92d31d209f809164eda43ff7
someone735 <smeone83@gmail.com>
2023-11-29 14:11:35 -0700

    update seat

--- seat.h
+++ seat.h
@@ -1,18 +1,23 @@
 #ifndef MY_SEAT_CLASS
 #define MY_SEAT_CLASS
+#include <string>
+using namespace std;
     class Seat{
         public:
             Seat();
             ~Seat();
-            Seat(Seat &src);
+            Seat(const Seat &src);
 
-            void setrow();
-            void setcolums();
+            void setrow(int rows);
+            void setcolumns(int columns);
+            void setseat_status(string input);
 
             int getrow() const;
             int getcolumns() const;
+            string get_seat_status() const;
         private:
             int rowsM;
             int columnsM;
+            string seat_status;
     };
 #endif
